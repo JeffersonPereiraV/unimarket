f064a2a0d02fca9e8202225cd4593a9e2e5fe028
Jefferson <jeffds126@gmail.com>
2025-05-22 15:42:27 -0300

    Organização das imagens

--- src/data/categories.tsx
+++ src/data/categories.tsx
@@ -1,40 +1,39 @@
-
 const categories = [
   {
     id: 1,
     name: "Apostilas",
-    icon: "https://images.unsplash.com/photo-1544947950-fa07a98d237f?q=80&w=400&auto=format"
+    icon: "https://images.unsplash.com/photo-1544947950-fa07a98d237f?q=80&w=400&auto=format",
   },
   {
     id: 2,
     name: "Doces",
-    icon: "https://images.unsplash.com/photo-1607920592519-aedf6e3b00fe?q=80&w=400&auto=format"
+    icon: "https://fly.metroimg.com/upload/q_85,w_700/https://uploads.metroimg.com/wp-content/uploads/2024/05/29105133/Foto-de-doces-e-guloseimas.jpg",
   },
   {
     id: 3,
     name: "Refeições",
-    icon: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?q=80&w=400&auto=format"
+    icon: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?q=80&w=400&auto=format",
   },
   {
     id: 4,
     name: "Artigos",
-    icon: "https://images.unsplash.com/photo-1550399105-c4db5fb85c18?q=80&w=400&auto=format"
+    icon: "https://images.unsplash.com/photo-1550399105-c4db5fb85c18?q=80&w=400&auto=format",
   },
   {
     id: 5,
     name: "Itens Pessoais",
-    icon: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?q=80&w=400&auto=format"
+    icon: "https://lebiscuit.vtexassets.com/arquivos/ids/11951847/17236335665978.jpg?v=638592413950530000",
   },
   {
     id: 6,
     name: "Eletrônicos",
-    icon: "https://images.unsplash.com/photo-1588508065123-287b28e013da?q=80&w=400&auto=format"
+    icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZewi8GaVSk3g3zqptZINeUo_PrWs8l_cp2g&s",
   },
   {
     id: 7,
     name: "Livros",
-    icon: "https://images.unsplash.com/photo-1495446815901-a7297e633e8d?q=80&w=400&auto=format"
-  }
+    icon: "https://images.unsplash.com/photo-1495446815901-a7297e633e8d?q=80&w=400&auto=format",
+  },
 ];
 
 export default categories;
